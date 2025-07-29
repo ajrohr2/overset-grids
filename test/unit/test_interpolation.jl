3800706515a0dc1bcc5b817a67a29967591b4d24
Aiden Rohrbach <arohrbachj@protonmail.com>
2025-07-29 07:09:13 -0400

    Update interpolation test

--- test/unit/test_interpolation.jl
+++ test/unit/test_interpolation.jl
@@ -1,4 +1,4 @@
-@testset "Interpolation" begin
+@testset "Interpolation 2D" begin
     square = UniformGrid2D((-1, -1), (1, 1), 0.02, :meg6)
     square_domain = square.iterators.cell.domain
     s_centroids_x, s_centroids_y = square.centroid_coordinates.x[square_domain], square.centroid_coordinates.y[square_domain]
@@ -16,7 +16,31 @@
     F_circle_exact = similar(F_circle)
     F_circle_exact .= f.(c_centroids_x, c_centroids_y)
 
-    interpolate_to_grid!(meshes[2][1], meshes[1][1], F_square, F_circle)
+    interpolate_to_grid2d!(meshes[2][1], meshes[1][1], F_square, F_circle)
+    error_matrix = error_estimate.(F_circle_exact, F_circle)
+    error_matrix[CartesianIndices((6:200-5, 6:200-5))] .= 0.0 
+
+    @test maximum(error_matrix) ≤ 1e-5
+end
+@testset "Interpolation 3D" begin
+    square = UniformGrid3D((-1, -1), (1, 1), 0.05, :meg6)
+    square_domain = square.iterators.cell.domain
+    s_centroids_x, s_centroids_y = square.centroid_coordinates.x[square_domain], square.centroid_coordinates.y[square_domain]
+
+    circle = rthetaphi_grid((0.25, 0, 0), (0.75, π, 2π), (50, 50, 50), :meg6)
+    circle_domain = circle.iterators.cell.domain
+    c_centroids_x, c_centroids_y = circle.centroid_coordinates.x[circle_domain], circle.centroid_coordinates.y[circle_domain]
+    meshes = create_components((square, circle), (2, 1))
+
+    f(x,y) = exp(sin(x) + cos(y))
+    F_square = similar(s_centroids_x)
+    F_square .= f.(s_centroids_x, s_centroids_y)
+
+    F_circle = similar(c_centroids_x)
+    F_circle_exact = similar(F_circle)
+    F_circle_exact .= f.(c_centroids_x, c_centroids_y)
+
+    interpolate_to_grid3d!(meshes[2][1], meshes[1][1], F_square, F_circle)
     error_matrix = error_estimate.(F_circle_exact, F_circle)
     error_matrix[CartesianIndices((6:200-5, 6:200-5))] .= 0.0 
 
